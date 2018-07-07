dad7b3ec625257910c9edc9bc88d11d85395a82f
Adelchi97 <elena.faillace97@gmail.com>
2018-07-07 12:22:14 +0200

    shared_ptr per gestire la window, posso gestirla in altre classi ora

--- source/World.cpp
+++ source/World.cpp
@@ -1,1 +1,1 @@
-//// Created by Elena Faillace on 07/07/2018.//#include "../include/World.h"World::World(sf::RenderWindow *window) : window(window){}
+//// Created by Elena Faillace on 07/07/2018.//#include "../include/World.h"World::World(std::shared_ptr<sf::RenderWindow> window, const TextureHolder& textures) : window(window), textures(textures) {}
