a444db6e635628b81448e12d49e18f0e83be33d7
Adelchi97 <elena.faillace97@gmail.com>
2018-09-19 07:49:31 +0200

    Test on RangedWeapon

--- test/TestHealPack.cpp
+++ test/TestHealPack.cpp
@@ -5,18 +5,17 @@
 #include "gtest/gtest.h"
 #include "../include/Objects/Healpack.h"
 
-//setting fixture
 struct HealPackTest: testing::Test {
 
-    //std::shared_ptr<World> world1 = std::make_shared<World>(window1, textures);
-
+    TextureHolder textures;
 };
+
 
 TEST_F(HealPackTest, Healpack_Costructor_Test) {
 
-    TextureHolder textures;
     textures.load(Textures::healPack,"../../Media/Objects/grayHealPack.png");
     Healpack healpack = Healpack(textures);
+
     EXPECT_EQ(healpack.active, true);
     EXPECT_EQ(healpack.equipped, false);
     EXPECT_EQ(healpack.counterLifeTime, 1);
@@ -25,22 +24,11 @@ TEST_F(HealPackTest, Healpack_Costructor_Test) {
 }
 
 TEST_F(HealPackTest, HealPackTest_Update_Test) {
-    TextureHolder textures;
+
     textures.load(Textures::healPack,"../../Media/Objects/grayHealPack.png");
     Healpack healpack = Healpack(textures);
+
     healpack.position = sf::Vector2f(42, -42);
     healpack.update();
     EXPECT_EQ(healpack.sprite.getPosition(), sf::Vector2f(42, -42));
 }
-
-/*
-TEST(TestOggetti, testConsistenzaCostruttore) {
-    std::cout << "Test consistenza costruttori.\n";
-
-    Oggetto oggettoTest(15., "Hatkanda", "L'oggetto di un film generico", 400);
-    EXPECT_EQ(oggettoTest.getPeso(), 15.);
-    EXPECT_EQ(oggettoTest.getNome(), "Hatkanda");
-    EXPECT_EQ(oggettoTest.getDescrizione(), "L'oggetto di un film generico");
-    EXPECT_EQ(oggettoTest.getValore(), 400);
-    EXPECT_GT(oggettoTest.attacca().getAmmontare(), 2.5); //ln(16) non so quanto faccia di preciso
-}*/
---
+++ test/TestRangedWeapon.cpp
@@ -0,0 +1,65 @@
+//
+// Created by Elena Faillace on 19/09/2018.
+//
+#include "gtest/gtest.h"
+#include "../include/Objects/RangedWeapon.h"
+
+struct RangedWeaponTest: testing::Test {
+
+    TextureHolder textures;
+};
+
+
+TEST_F(RangedWeaponTest, RangedWeapon_Costructor_Test) {
+
+    textures.load(Textures::redShooter,"../../Media/Objects/enemyGun.png");
+    RangedWeapon rangedWeapon(textures, RangedWeapon::Type::redShooter);
+
+    EXPECT_EQ(rangedWeapon.typeWeapon, RangedWeapon::Type::redShooter);
+    EXPECT_EQ(rangedWeapon.projectileType, Projectile::Type::redProjectile);
+    EXPECT_EQ(rangedWeapon.type, Object::rangedWeapon);
+    EXPECT_EQ(rangedWeapon.equipped, false);
+    EXPECT_EQ(rangedWeapon.active, true);
+    EXPECT_EQ(rangedWeapon.range, 400);
+    EXPECT_EQ(rangedWeapon.power, 10);
+    EXPECT_EQ(rangedWeapon.projectilesCounter, 40);
+
+    //Test different weapon
+    textures.load(Textures::energyShooter,"../../Media/Objects/gun.png");
+    RangedWeapon rangedWeapon2(textures, RangedWeapon::Type::energyShooter);
+    EXPECT_EQ(rangedWeapon2.typeWeapon, RangedWeapon::Type::energyShooter);
+    EXPECT_EQ(rangedWeapon2.projectileType, Projectile::Type::energyBall);
+}
+
+TEST_F(RangedWeaponTest, RangedWeapon_AddStuff_Test) {
+
+    textures.load(Textures::energyShooter,"../../Media/Objects/gun.png");
+    RangedWeapon rangedWeapon(textures, RangedWeapon::Type::energyShooter);
+    rangedWeapon.addStuff(1200);
+    EXPECT_EQ(rangedWeapon.projectilesCounter, 1240);
+
+    RangedWeapon rangedWeapon2(textures, RangedWeapon::Type::energyShooter);
+    rangedWeapon2.addStuff(-20);
+    EXPECT_EQ(rangedWeapon2.projectilesCounter, 40);
+}
+
+TEST_F(RangedWeaponTest, RangedWeapon_Use_Test) {
+    textures.load(Textures::energyShooter,"../../Media/Objects/gun.png");
+    RangedWeapon rangedWeapon(textures, RangedWeapon::Type::energyShooter);
+
+    rangedWeapon.use();
+    EXPECT_EQ(rangedWeapon.projectilesCounter, 39);
+    rangedWeapon.projectilesCounter = 0;
+    EXPECT_EQ(rangedWeapon.use(), false);
+}
+
+TEST_F(RangedWeaponTest, RangedWeapon_ChangeType_Test) {
+    textures.load(Textures::redShooter,"../../Media/Objects/enemyGun.png");
+    textures.load(Textures::energyShooter,"../../Media/Objects/gun.png");
+
+    RangedWeapon rangedWeapon(textures, RangedWeapon::Type::redShooter);
+
+    rangedWeapon.changeType(RangedWeapon::Type::energyShooter);
+    EXPECT_EQ(rangedWeapon.typeWeapon, RangedWeapon::Type::energyShooter);
+    EXPECT_EQ(rangedWeapon.projectileType, Projectile::Type::energyBall);
+}
